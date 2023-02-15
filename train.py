ecf5da8d6932aa6dde80df0d6c1c2c0ac540b6b5
j4ix <jai@j4ix.com>
2023-02-15 04:31:27 -0500

    it went back over train.py

--- train.py
+++ train.py
@@ -17,6 +17,7 @@ batch_size = 64
 epochs = 1000
 noise_dim = 100
 seed_mp3s = ['/path/to/seed1.mp3', '/path/to/seed2.mp3', ...]
+output_dir = '/path/to/output/directory'
 
 # load MP3 dataset
 dataset = load_mp3_dataset(data_dir)
@@ -25,4 +26,28 @@ dataset = load_mp3_dataset(data_dir)
 gan = GAN()
 
 # train GAN model
-gan.train(dataset, epochs=epochs, batch_size=batch_size, seed_mp3s=seed_mp3s)
+for epoch in range(epochs):
+    for batch in dataset:
+        # generate input noise for generator
+        noise = tf.random.normal([batch_size, noise_dim])
+
+        # train discriminator
+        real_mp3s = batch
+        fake_mp3s = gan.generator(noise)
+        gan.train_discriminator(real_mp3s, fake_mp3s)
+
+        # generate new samples every 100 epochs
+        if epoch % 100 == 0:
+            generated_mp3s = gan.generate_samples(num_samples=5, seed_mp3=seed_mp3s, output_dir=output_dir, epoch=epoch)
+
+        # train generator
+        noise = tf.random.normal([batch_size, noise_dim])
+        gan.train_generator(noise)
+
+    # display training progress every 10 epochs
+    if epoch % 10 == 0:
+        print(f'Epoch {epoch}/{epochs}')
+        print(f'Discriminator Loss: {gan.discriminator_loss.result()}')
+        print(f'Generator Loss: {gan.generator_loss.result()}')
+        gan.discriminator_loss.reset_states()
+        gan.generator_loss.reset_states()
