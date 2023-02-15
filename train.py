736881d36fd08cdcc141959b677a8b71fd23db3d
j4ix <jai@j4ix.com>
2023-02-15 04:18:52 -0500

    ..

--- train.py
+++ train.py
@@ -11,4 +11,18 @@ if gpus:
     except RuntimeError as e:
         print(e)
 
-# set up hyper
+# set up hyperparameters
+data_dir = '/path/to/mp3/data'
+batch_size = 64
+epochs = 1000
+noise_dim = 100
+seed_mp3s = ['/path/to/seed1.mp3', '/path/to/seed2.mp3', ...]
+
+# load MP3 dataset
+dataset = load_mp3_dataset(data_dir)
+
+# instantiate GAN model
+gan = GAN()
+
+# train GAN model
+gan.train(dataset, epochs=epochs, batch_size=batch_size, seed_mp3s=seed_mp3s)
