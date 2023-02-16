42dbeb6313fcefed0a69da140b2eb5aea207fddb
j4ix <jai@j4ix.com>
2023-02-15 22:11:51 -0500

    .

--- train.py
+++ train.py
@@ -30,3 +30,28 @@ for epoch in range(EPOCHS):
             # Train discriminator on real audio
             real_audio = batch
             real_output = discriminator(real_audio)
+            # Train discriminator on fake audio
+            fake_output = discriminator(fake_audio)
+
+            # Calculate discriminator loss
+            disc_loss = loss_fn(tf.ones_like(real_output), real_output) + \
+                        loss_fn(tf.zeros_like(fake_output), fake_output)
+
+            # Calculate generator loss
+            gen_loss = loss_fn(tf.ones_like(fake_output), fake_output)
+
+            # Calculate gradients and apply to optimizer
+            gen_grads = gen_tape.gradient(gen_loss, generator.trainable_variables)
+            disc_grads = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+            optimizer.apply_gradients(zip(gen_grads, generator.trainable_variables))
+            optimizer.apply_gradients(zip(disc_grads, discriminator.trainable_variables))
+
+# Print loss values and other metrics
+print(f"Generator loss: {gen_loss:.4f}")
+print(f"Discriminator loss: {disc_loss:.4f}")
+
+# Generate new mp3 every 100 epochs
+if epoch % 100 == 0:
+    seed_audio = [load_audio_file(mp3_file) for mp3_file in SEED_MP3S]
+    generated_audio = generator(tf.random.normal([1, 100]))
+    save_audio_file(generated_audio, f"generated_audio_epoch{epoch}.mp3")
